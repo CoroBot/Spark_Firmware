e5087ac359159c289f72d71f01af7ad3b91ed571
jricher42 <jacques@richer.email>
2015-08-13 12:24:48 -0700

    Wrote classes for USB HID communications and spark control
    Building library and test application
    Wrote a hidcomm class for USB HID communications with Spark board
    Wrote a spark_drive class which is protocol agnostic, and contains
    methods and constants for driving Spark.
    Major update

--- software/Python/Test_Spark.py
+++ software/Python/Test_Spark.py
@@ -3,7 +3,9 @@
 # Script for LED control demo. 
 
 from cobs_serial import cobs_serial
+import sys
 import struct
+import platform
 import hid
 
 #USB Constants
@@ -28,47 +30,158 @@ Option Menu:
   3. Set the first/front motor speed
   4. Set servo duty cycle
   5. Request ultrasonic distance
-  6. Exit
+  6. Test HID Loopback
+  X. Exit
 
   Please Enter a number from above menu >>"""
 
+class HID_Comm(object):
+	def __init__(self):
+		self.hiddev = hid.device()
+		self.prefix = []
+		if platform.system() == 'Windows':
+			self.prefix=[0x80]
+		self.isopen=False
 
-def main():
+	def open(self, vid=coroware_VID, pid=spark_PID):
+		if self.isopen:
+			raise ValueError, "Already Open"
+		self.hiddev.open(vid,pid)
+		self.isopen = True
+
+	def close(self):
+		if not self.isopen:
+			raise ValueError, "Not Open"
+		self.hiddev.close()
+		self.isopen = False
+
+	def send(self, data):
+		if not self.isopen:
+			raise IOError, "Not Open"
+		prefix = self.prefix[:]
+		prefix.extend(list(data))
+
+	def receive(self, numbytes=64, blocking=True):
+		if not self.isopen:
+			raise IOError, "Not Open"
+		if not blocking:
+			self.hiddev.set_nonblocking(1)
+		data = self.hiddev.read(numbytes)
+		if not blocking:
+			self.hiddev.set_nonblocking(0)
+		return data
+
+	def send_frame(self, unit, subunit, command, data):
+		if not self.isopen:
+			raise IOError, "Not Open"
+		out = struct.pack("BBBs", unit, subunit, command, data)
+		if len(out)>64:
+			raise ValueError
+		self.send(out)
+		
+	def set_value(self, unit, subunit, setting, value):
+		command_set_value = 1 
+		additional = struct.pack(">H>H", setting, value)
+		comm.send_frame(unit, subunit, command_set_value, additional)
+		
+
+	def receive_frame(self, blocking=True):
+		if not self.isopen:
+			raise IOError, "Not Open"
+		data = self.receive(64, blocking)
+		if len(data) < 3:
+			raise IOError, "Invalid Frame: Too short"
+		unit = data[0] - 0x80
+		if unit < 0:
+			raise IOError, "Invalid Frame: Invalid Unit #"
+		subunit = data[1]
+		data_type = data[2]
+		additional = data[3:]
+		return (unit, subunit, data_type, additional)
+		
+	def get_value(self, unit, subunit, setting):
+		command_get_value = 0 
+		additional = struct_pack(">H", setting)
+		self.send_frame(unit, subunit, command_get_value, additional)
+		runit, rsubunit, rsetting, radditional = self.receive_frame()
+		rsetting, rvalue = struct.unpack(">H>H")
+		if unit <> runit or subunit <> rsubunit or setting <> rsetting:
+			raise IOError, "Invalid Frame: data mismatch"
+		return rvalue
+
+		
+class Spark_Drive(object):
+	def __init__(self, comm):
+		self.comm = comm
+		self.unit_loopback = 0
+		self.unit_motor = 1
+		self.unit_led = 2
+		self.unit_servo = 4
+		self.unit_adc = 4
+		self.unit_ultrasonic = 5
+		
+		self.setting_pwm = 1
+		self.setting_direction = 2
+		self.setting_ADC = 3 
+		self.setting_USonic = 4 
+
+		self.value_stopped = 0
+		self.value_forward = 1
+		self.value_reverse = 2
+		
+		self.command_set = 1
+		self.command_read = 0
+		
+	def set_motor_speed(self, motornum, speed):
+		comm.set_value(self.unit_motor, motornum, self.setting_pwm, speed)
+	
+	def set_motor_direction(self, motornum, direction):
+		comm.set_value(self.unit_motor, motornum, self.setting_direction, direction)
+
+	def set_led_brightness(self, lednum, brightness):
+		comm.set_value(self.unit_led, lednum, self.setting_pwm, brightness)
+		
+	def set_servo_width(self, servonum, width):
+		comm.set_value(self.unit_servo, servonum, self.setting_pwm, width)
+	
+	def get_adc(self, adcnum):
+		return comm.get_value(self.unit_adc, adcnum, self.setting_ADC)
+	
+	def get_ultrasonic(self, ultrasonic_num):
+		return comm.get_value(self.unit_ultrasonic, ultrasonic_num, self.setting_USonic)
+  
+ def main():
+	hidcomm = HID_Comm()
+	try:
+		hidcomm.open()
+	except IOError
+		print "Spark not found"
+		sys.exit()
+
+	spark = Spark_Drive(hidcomm)
+	
 	print menu_header
 	
-	# Open the serial port
-	# ************************** USER ENTERED PORT ***************************
-	port = raw_input("Enter your serial port (EX: COM14)\n>>")
-	cobs = cobs_serial(port, 115200, 1)
-	
-	# ************************** DEFAULT DEBUG PORT ***************************
-	#print "Defaulting port to COM14. Change script if neeeded"
-	#cobs = cobs_serial('COM14', 115200, 1) #apparently the port is offset by 1 (so COM14 is 13)
-	
-	# *************************** END PORT CONFIG ****************************
-	
-	#User Menu
-		
 	#Loop until user quits	
 	while True:
 		option = raw_input(menu)
 		sel = option.strip()
-		if sel == '6':
+		if sel == 'X':
 			break
 		elif sel == '1': #CONTROL LEDS
-			do_led(cobs)
+			do_led(comm)
 		elif sel == '2': #READ ADC VALUE
-			do_adc(cobs)
+			do_adc(comm)
 		elif sel == '3': #TEST MOTORS
-			do_motor(cobs)
+			do_motor(comm)
 		elif sel == '4': #CONTROL SERVOS
-			do_servo(cobs)
+			do_servo(comm)
 		elif sel == '5': #READ ULTRASONIC SENSOR
-			do_ultrasonic(cobs)
+			do_ultrasonic(comm)
 
 				
 				
-def do_led(cobs):
+def do_led(comm):
 	print "LED Control"
 	option = raw_input("Enter an LED to control, 1-3\n>>")
 	try:
