8e2297ceaf30e546a9a31bb38326958c06b900a5
MetaEmpire <kfejeran@uw.edu>
2015-08-13 20:19:12 -0700

    Deleted commented code in main, updated script
    The project now resembles Jacques new format for Creator projects. The
    main file is very empty except for a single hardware initialization
    function.
    
    The python script has also had some light havoc testing, and bugs
    revealed by these tests fixed.

--- software/Python/psoc4_test.py
+++ software/Python/psoc4_test.py
@@ -53,12 +53,12 @@ def main():
 	
 	# Open the serial port
 	# ************************** USER ENTERED PORT ***************************
-	#port = raw_input("Enter your serial port (EX: COM14)\n>>")
-	#cobs = cobs_serial(port, 115200, 1)
+	port = raw_input("Enter your serial port (EX: COM14)\n>>")
+	cobs = cobs_serial(port, 115200, 1)
 	
 	# ************************** DEFAULT DEBUG PORT ***************************
-	print "Defaulting port to COM17. Change script if neeeded"
-	cobs = cobs_serial('COM17', 115200, 1)
+	#print "Defaulting port to COM17. Change script if neeeded"
+	#cobs = cobs_serial('COM17', 115200, 1)
 	
 	# *************************** END PORT CONFIG ****************************
 	
@@ -90,7 +90,7 @@ def main():
 		if type == 7:
 			break
 		elif type == 0: #DIRECTION
-			do_direction(motornum, type, cobs, True)
+			do_direction(motornum, type, cobs)
 		elif type == 1: #SPEED
 			do_speed(motornum, type, cobs)
 		elif type == 2: #ENCODER
@@ -104,6 +104,7 @@ def main():
 		elif type == 6: #STOP MOTORS
 			stopMotors(cobs)
 
+#fires a message to set both motor speeds to zero.
 def stopMotors(cobs):
 	cmd = bytearray([0x3, 0x1, 0x1, 0x0, 0x0])
 	writeToSerial(cmd, cobs)
@@ -116,9 +117,11 @@ def getNumInRange(min = 0, max = 65535, prompt="Please enter a number >>"):
 		returnMe = int(raw_input(prompt))
 	except:
 		print "Error: The input cannot be converted to an integer."
+		raw_input("Press enter to continue...")
 		return -1
 	if (returnMe < min or returnMe > max): 
 		print "Error: The input is out of range: " + repr(min) + ", " + repr(max)
+		raw_input("Press enter to continue...")
 		return -1
 	return returnMe
 	
@@ -128,6 +131,7 @@ def writeToSerial(buf, cobs):
 	except:
 		print "Error: Could not write to serial. Try power cycling board."
 		print "Additional Info: Buffer contents " + repr(list(buf))
+		raw_input("Press enter to continue...")
 
 def getResponse(cobs, debug = False):
 	retarray = cobs.block_and_return()
@@ -141,6 +145,7 @@ def getResponse(cobs, debug = False):
 		#print "Direction of motor #" + repr(motornum) + ": " + repr(int(direction_returned[0]))
 	except:
 		print "Error converting incoming value to an int."	
+		raw_input("Press enter to continue...")
 		return -1
 
 def prepareCommand(motornum, getset, option):
@@ -152,6 +157,8 @@ def prepareCommand(motornum, getset, option):
 		
 def do_direction(motornum, type, cobs, debug = False):
 	getset = getNumInRange(0,1, getset_menu)
+	if getset == -1:
+		return
 	cmd = prepareCommand(motornum, getset, 0) #0 for option direction
 	
 	if getset == 1:
@@ -178,6 +185,8 @@ def do_direction(motornum, type, cobs, debug = False):
 		
 def do_speed(motornum, type, cobs, debug = False):
 	getset = getNumInRange(0,1, getset_menu)
+	if getset == -1:
+		return	
 	cmd = prepareCommand(motornum, getset, 1) 
 	
 	if getset == 1:
@@ -239,6 +248,8 @@ def do_current(motornum, type, cobs, debug = False):
 	
 def do_mode(motornum, type, cobs, debug = False):
 	getset = getNumInRange(0,1, getset_menu)
+	if getset == -1:
+		return	
 	cmd = prepareCommand(motornum, getset, 4) 
 	
 	if getset == 1:
@@ -264,6 +275,8 @@ def do_mode(motornum, type, cobs, debug = False):
 
 def do_power(motornum, type, cobs, debug = False):
 	getset = getNumInRange(0,1, getset_menu)
+	if getset == -1:
+		return	
 	cmd = prepareCommand(motornum, getset, 5)
 	
 	if getset == 1:
@@ -286,114 +299,6 @@ def do_power(motornum, type, cobs, debug = False):
 			print "Power state of motor #" + repr(motornum) + ": " + repr(response)
 			
 	raw_input("\nPress enter to continue...")
-		
-		
-		
-
-#################### OLD FUNCTIONS #########################		
-		
-def do_led(cobs):
-	print "LED Control"
-	option = raw_input("Enter an LED to control, 1-3\n>>")
-	try:
-		lednum = int(option.strip())
-		if lednum < 1 or lednum > 3:
-			print "Number out of range"
-			return
-		numbytes = struct.pack('>H', lednum) #H is for unsigned short
-	except:
-		print "Error converting number"
-		return
-	
-	option = raw_input("Enter a new compare value for the LED duty cycle, 0-65535\n>>")
-	try:
-		ledval = int(option.strip())
-		valbytes = struct.pack('>H', ledval)
-	except:
-		print "Error converting value"
-		return
-				
-	bob = bytearray([0x01, 0x01, 0x04])
-	bob += numbytes
-	bob += valbytes
-	print "Sending bytes to encode and send: " + repr(list(bob)) #for debugging
-	cobs.encode_and_send(bob) #confirm baud rate and com port
-
-def do_adc(cobs):
-	#adc req
-	#print "Setting LED1 to the result of ADC Read"
-	sam = bytearray([0x01, 0x08, 0x00])
-	cobs.encode_and_send(sam)
-	#print "Reading raw bytes from serial response"
-	retarray = cobs.block_and_return()
-	#print "Cobs decode result array: "
-	#print repr(retarray)
-	try:
-		adcval = struct.unpack('H', retarray)
-		print "ADC val :"
-		print int(adcval[0])
-	except:
-		print "Error converting adc value"
-
-def do_motor(cobs):
-	#motor speed
-	
-	option = raw_input("Enter a new compare value for the motor duty cycle, 0-65535\n>>")
-	try:
-		val = int(option.strip())
-		valbytes = struct.pack('>H', val)
-	except:
-		print "Error converting value"
-		return
-						
-	print "Sending command to the psoc5, commanding it to talk to the psoc4"
-	cmd = bytearray([0x01, 0x01, 0x04])
-	cmd += struct.pack('>H', 5)
-	cmd += valbytes
-	#print "debug: array to encode: " + repr(cmd)
-	print "debug: sending array to encode and send routine"
-	cobs.encode_and_send(cmd)
-		
-def do_servo(cobs):
-	#
-	print "Servo Control \n(see script to change min and max pulse widths. Currently defaulted to a TowerPro MG995 Servo)"
-	option = raw_input("Enter a percentage >>")
-	try:
-		percent = float(option.strip())
-		if percent < 0 or percent > 100:
-			print "Error, Value not between 0 and 100."
-			return
-		pwmcompare = ((percent/100) * (maxpulse - minpulse)) + minpulse
-		pwmcompare = servo_period - pwmcompare #left align the positive pulse
-		#print "debug pwmcompare final value before conversion:" + repr(pwmcompare)
-		comparebytes = struct.pack('>H', pwmcompare) #H is for unsigned short
-	except:
-		print "Error converting number"
-		return
-	
-	numbytes = struct.pack('>H', 4) #H is for unsigned short
-	
-	bytestosend = bytearray([0x01, 0x01, 0x04])
-	bytestosend += numbytes
-	bytestosend += comparebytes
-	print "Sending bytes to encode and send: " + repr(list(bytestosend)) #for debugging
-	cobs.encode_and_send(bytestosend) #confirm baud rate and com port
-
-def do_ultrasonic(cobs):	
-	#timer req
-	#print "Setting LED1 to the result of ADC Read"
-	sam = bytearray([0x01, 0x09, 0x00])
-	cobs.encode_and_send(sam)
-	#print "Reading raw bytes from serial response"
-	retarray = cobs.block_and_return()
-	print "Cobs decode result array: "
-	#print repr(retarray)
-	try:
-		timerval = struct.unpack('H', retarray)
-		print "Timer val :"
-		print int(timerval[0])
-	except:
-		print "Error converting timer value"
 		
 if __name__ == '__main__':
 	main()
