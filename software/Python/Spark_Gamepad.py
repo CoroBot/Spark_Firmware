142ad5a8a6e2e0c639fbf9b0c0b037bf840d7abf
ssv normandy <rcircuit96@gmail.com>
2015-12-01 19:23:23 +0000

    gamepad: added relative view mode to camera control, switch modes by clicking right stick, change movement rate by holding left button

--- software/Python/Spark_Gamepad.py
+++ software/Python/Spark_Gamepad.py
@@ -5,9 +5,12 @@ from Spark_Control import HID_Comm, NET_ZMQ_Comm, Spark_Drive
 
 pygame.init()
 
-deadzone = 300
+deadzone = 0.05
 servoCenter = 18700
 servoRange = 1000
+holdPosition = True
+tilt = servoCenter
+pan = servoCenter
 
 pygame.joystick.init()
 
@@ -51,6 +54,7 @@ def main():
 def two_stick(spark, joystick):
         global deadzone
         global servoCenter
+        global servoRange
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done=True
@@ -85,37 +89,90 @@ def one_stick(spark, joystick):
         global deadzone
         global servoCenter
         global servoRange
+        global holdPosition
+        global pan
+        global tilt
+        panRate = 20
+        tiltRate = 20
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         done=True
+                if joystick.get_button(10) == 1 and event.type == pygame.JOYBUTTONDOWN:
+                        holdPosition = not holdPosition
+                        tilt = servoCenter
+                        pan = servoCenter
+                        
         LRjoystick = joystick.get_axis(0)
+        if abs(LRjoystick) < deadzone:
+                LRjoystick = 0
+                
         FBjoystick = joystick.get_axis(1)
+        if abs(FBjoystick) < deadzone:
+                FBjoystick = 0
+                
         cameraPan = joystick.get_axis(3)
+        if abs(cameraPan) < deadzone:
+                cameraPan = 0
+                
         cameraTilt = joystick.get_axis(4)
+        if abs(cameraTilt) < deadzone:
+                cameraTilt = 0
+                
         leftval = FBjoystick-LRjoystick
         rightval = FBjoystick+LRjoystick
+        lookrate = joystick.get_button(4)
+        
+        if lookrate == 1:
+                panRate = 2
+                tiltRate = 2
+                
         if leftval < 0:
                 spark.set_motor_direction(5,0)
+                
         else:
                 spark.set_motor_direction(5,1)
+                
         if rightval < 0:
                 spark.set_motor_direction(6,0)
+                
         else:
                 spark.set_motor_direction(6,1)
+                
         leftpwm = abs(leftval)*65535
         rightpwm = abs(rightval)*65535
-        if leftpwm < deadzone:
-                leftpwm = 0
-        if rightpwm < deadzone:
-                rightpwm = 0
         if leftpwm > 65535:
                 leftpwm = 65535
+                
         if rightpwm > 65535:
                 rightpwm = 65535
+                
         spark.set_motor_speed(5, leftpwm)
         spark.set_motor_speed(6, rightpwm)
-        spark.set_servo_width(1,servoCenter+(servoRange*cameraTilt))
-        spark.set_servo_width(2,servoCenter+(servoRange*cameraPan))
+        if holdPosition == False:
+                spark.set_servo_width(1,servoCenter+(servoRange*cameraTilt))
+                spark.set_servo_width(2,servoCenter+(servoRange*cameraPan))
+                
+        else:
+                tilt = tilt+(cameraTilt*tiltRate)
+                pan = pan+(cameraPan*panRate)
+                if tilt >= (servoCenter-servoRange) and tilt <= (servoCenter+servoRange):
+                        spark.set_servo_width(1,tilt)
+                        
+                elif tilt < (servoCenter-servoRange):
+                        tilt = (servoCenter-servoRange)
+                        
+                elif tilt > (servoCenter+servoRange):
+                        tilt = (servoCenter+servoRange)
+                        
+                if pan >= (servoCenter-servoRange) and pan <= (servoCenter+servoRange):
+                        spark.set_servo_width(2,pan)
+                        
+                elif pan < (servoCenter-servoRange):
+                        pan = (servoCenter-servoRange)
+                        
+                elif pan > (servoCenter+servoRange):
+                        pan = (servoCenter+servoRange)
+                        
         if joystick.get_button(6) == 1 and joystick.get_button(7) == 1:
                 pygame.quit()
                 sys.exit()
